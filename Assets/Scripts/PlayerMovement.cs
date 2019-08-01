316fb6f6cbd5edd5ad3f1bc2292630d77797dc21
james <james@jungledisk.com>
2019-08-01 11:58:30 -0500

    first commit, project complete

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float turnSpeed = 20f;
+
+    Animator m_Animator;
+    Rigidbody m_Rigidbody;
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+    AudioSource m_AudioSource; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //get input
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        //set movement vector
+        m_Movement.Set(horizontal, 0f, vertical); // sets movement vector per frame
+        m_Movement.Normalize(); // won't let x y or z go past 1, thanks pythagorus
+
+        //checking for input to see if char is walking
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f); // true if horizontal not 0
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f); //true if vertical not 0
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool("IsWalking", isWalking);
+
+        if (isWalking)
+        {
+            if (!m_AudioSource.isPlaying)
+            {
+                m_AudioSource.Play();
+            }
+        }
+        else
+        {
+            m_AudioSource.Stop();
+        }
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f); // calculates character's forward vector
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    void OnAnimatorMove()
+    {
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MoveRotation(m_Rotation);
+    }
+}
---
+++ Assets/Scripts/WaypointPatrol.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WaypointPatrol : MonoBehaviour
+{
+
+    public NavMeshAgent navMeshAgent;
+    public Transform[] waypoints;
+    int m_CurrentWaypointIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent.SetDestination(waypoints[0].position); //moves object to waypoint 0 at load
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance) // check to see if object has gotten to the waypoint
+        {
+            m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length; // changes the current waypoint index. the code is saying 
+                                                                                      // "Add one to the current index, but if that increment puts 
+                                                                                      // the index equal to the number of elements in the waypoints 
+                                                                                      // array then instead set it to zero."
+            navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
+        }
+    }
+}
